fa6769998ab28a4bd1492c54a48c4e7472ef6902
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-27 15:22:41 +0100

    01 Gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const galleryBox = document.querySelector("ul.gallery");
+
+for (const item of galleryItems) {
+    const imgGallery = `<li class="gallery__item"><a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a></li>`;
+    galleryBox.insertAdjacentHTML("beforeend", imgGallery);
+}
+const lightboxInstance = new SimpleLightbox(".gallery__item a", {
+    captions: true,
+    captionDelay: 250,
+    captionSelector: "img",
+    captionsData: "alt",
+    captionPosition: "bottom",
+});
 
 console.log(galleryItems);
