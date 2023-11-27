b3a30ca58acaf0f54f4e23925f424c20b9eeab6e
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-27 15:23:16 +0100

    03 feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import _ from 'lodash';
 
+const document.querySelector('[name="email"]')
+    .setAttribute('oninput', 'throttledFunction()');
+
+function myFunc2() {
+    let value = document.getElementById("myInput").value;
+    localStorage.setItem('value', value);
+    console.log(value);
+}
+
+const throttledFunction = _.throttle(myFunc2, 1000);
