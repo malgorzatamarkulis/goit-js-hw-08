4989915f8365133bd17ca54d8adf25af86fbfc8f
MalgorzataM <malgorzata.markulis@gmail.com>
2023-12-05 21:09:20 +0100

    Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,24 @@
+// Add imports above this line
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.css';
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-const galleryBox = document.querySelector("ul.gallery");
+const galleryBox = document.querySelector('ul.gallery');
 
 for (const item of galleryItems) {
     const imgGallery = `<li class="gallery__item"><a class="gallery__link" href="${item.original}">
      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
    </a></li>`;
-    galleryBox.insertAdjacentHTML("beforeend", imgGallery);
+    galleryBox.insertAdjacentHTML('beforeend', imgGallery);
 }
-const lightboxInstance = new SimpleLightbox(".gallery__item a", {
+const lightboxInstance = new SimpleLightbox('.gallery__item a', {
     captions: true,
     captionDelay: 250,
-    captionSelector: "img",
-    captionsData: "alt",
-    captionPosition: "bottom",
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
 });
+
+// Change code below this line
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,44 @@
-import _ from 'lodash';
+import throttle from 'lodash.throttle';
 
-const document.querySelector('[name="email"]')
-    .setAttribute('oninput', 'throttledFunction()');
+const form = document.querySelector('form');
+const emailInput = document.querySelector("input[name='email']");
+const messageInput = document.querySelector("textarea[name='message']");
 
-function myFunc2() {
-    let value = document.getElementById("myInput").value;
-    localStorage.setItem('value', value);
-    console.log(value);
-}
+// Load
+document.addEventListener('DOMContentLoaded', () => {
+    const storedData = localStorage.getItem('feedback-form-state');
+    if (storedData) {
+        const { email, message } = JSON.parse(storedData);
+        emailInput.value = email;
+        messageInput.value = message;
+        console.log(storedData);
+    }
+});
 
-const throttledFunction = _.throttle(myFunc2, 1000);
+// Tracking
+const saveToLocalStorage = throttle(() => {
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+    const formData = {
+        email: emailValue,
+        message: messageValue,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+
+form.addEventListener('input', saveToLocalStorage);
+
+// Remove
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    // Log form data
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+    console.log({
+        email: emailValue,
+        message: messageValue,
+    });
+    localStorage.removeItem('feedback-form-state');
+    console.log('Clear localStorage');
+    form.reset();
+});
